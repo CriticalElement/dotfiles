0ed03499a20733ce6e8c859e17d29e107d39d38e
Pranav <pranav.pakanati@gmail.com>
2024-08-22 00:40:46 -0500

    improve space animation on first load

--- .config/sketchybar/scripts/highlight_space.sh
+++ .config/sketchybar/scripts/highlight_space.sh
@@ -1,3 +1,6 @@
 SID=$(echo $INFO | jq .[\"display-1\"])
+if [ "$SID" = "" ]; then
+    SID=$(yabai -m query --spaces | jq '.[] | select(.["has-focus"] == true).index')
+fi
 LENGTH=$($(echo yabai -m query --spaces) | jq length)
 sketchybar --animate circ 15 --set highlight_space background.padding_left=$((-(LENGTH - (SID - 1)) * 30 + 4))

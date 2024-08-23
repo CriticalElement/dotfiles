f8f6856759d9d2237c27ffe9a87b2eb47028c7c8
Pranav <pranav.pakanati@gmail.com>
2024-08-22 23:20:01 -0500

    add title bar to sketchybar

--- .config/sketchybar/scripts/highlight_space.sh
+++ .config/sketchybar/scripts/highlight_space.sh
@@ -1,6 +1,6 @@
 SID=$(echo $INFO | jq .[\"display-1\"])
 if [ "$SID" = "" ]; then
-    SID=$(yabai -m query --spaces | jq '.[] | select(.["has-focus"] == true).index')
+    SID=$(yabai -m query --spaces --space | jq '.index')
 fi
 LENGTH=$($(echo yabai -m query --spaces) | jq length)
 sketchybar --animate circ 15 --set highlight_space background.padding_left=$((-(LENGTH - (SID - 1)) * 30 + 4))

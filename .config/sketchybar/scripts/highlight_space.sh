b9ac5e4949602100728a8f1fc6f48785477e502b
Pranav <pranav.pakanati@gmail.com>
2024-08-24 00:24:37 -0500

    add space button

--- .config/sketchybar/scripts/highlight_space.sh
+++ .config/sketchybar/scripts/highlight_space.sh
@@ -3,4 +3,4 @@ if [ "$SID" = "" ]; then
     SID=$(yabai -m query --spaces --space | jq '.index')
 fi
 LENGTH=$($(echo yabai -m query --spaces) | jq length)
-sketchybar --animate circ 15 --set highlight_space background.padding_left=$((-(LENGTH - (SID - 1)) * 30 + 4))
+sketchybar --animate circ 15 --set highlight_space background.padding_left=$((-(LENGTH - (SID - 2)) * 30 + 7))

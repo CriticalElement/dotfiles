52f48024f671f0632fd7c699044a287441090d6a
Pranav <pranav.pakanati@gmail.com>
2025-01-11 01:06:40 -0600

    fallback to app title when window title is blank

--- .config/sketchybar/scripts/title.sh
+++ .config/sketchybar/scripts/title.sh
@@ -5,7 +5,9 @@ if [ "$TITLE" = "" ]; then
     sleep 0.2 && sketchybar --set title label=""
 else
     LABEL="$(echo $TITLE | jq -r '.title')"
-    echo $LABEL
+    if [ "$LABEL" = "" ]; then
+        LABEL="$(echo $TITLE | jq -r '.app')"
+    fi
     if [ "$(sketchybar --query title_proxy | jq -r '.label.value')" != "$LABEL" ]; then
         sketchybar --set title_proxy label="$LABEL"
         sketchybar --animate circ 15 --set title y_offset=70            \
